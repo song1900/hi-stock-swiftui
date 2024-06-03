4a7f59658e5ee954bdec228c15e2ef61d47318f0
Woojin Song <woojin1900@gmail.com>
2024-06-03 21:22:32 +0900

    Test: github actions 설정을 위한 테스트코드

---
+++ HiStock/HiStockTests/HiStockTests.swift
@@ -0,0 +1,16 @@
+//
+//  HiStockTests.swift
+//  HiStockTests
+//
+//  Created by 송우진 on 6/3/24.
+//
+
+import XCTest
+
+final class HiStockTests: XCTestCase {
+
+    func testExample() {
+        XCTAssert(true)
+    }
+
+}
