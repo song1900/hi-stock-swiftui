d82be5bb60f80e45997aae04433b2a6596287f50
Woojin Song <woojin1900@gmail.com>
2024-06-21 23:30:06 +0900

    검색 기능 추가 (#7)
    * Feat: 디코딩을 위한 Stock 구조체 수정
    
    - 모든 값을 nil을 허용하도록 변경
    - 디코딩 과정 중 실패할 경우 nil을 반환
    - MarketType Codable
    
    * Feat: FirestoreManager 생성
    
    - Firestore를 사용하는 메서드를 포함하는 클래스 생성
    - Protocol을 통해 접근 하도록 Protocol 생성
    
    * Feat: FirestoreClient 생성
    
    FirestoreManager를 사용할 수 있는 DependencyClient
    
    * Feat: Search Reducer & View 기능 추가
    
    - FirestoreClient Dependency 추가
    - 검색 액션 추가
    - performSearch 에러, 성공시 print 출력

--- HiStock/HiStock/Domain/Entities/Stock.swift
+++ HiStock/HiStock/Domain/Entities/Stock.swift
@@ -7,12 +7,42 @@
 
 import Foundation
 
-struct Stock: Equatable, Identifiable {
-    var id: String { return code }
-    var title: String
-    var price: Int
-    var code: String
-    var market: MarketType
+struct Stock: Codable, Equatable, Identifiable {
+    var id: Int { code?.hashValue ?? 0 }
+    var title: String?
+    var code: String?
+    var price: Int?
+    var market: MarketType?
     var fluctuationRate: Double?
-    var thema: [String]?
+    var themas: [String]?
+    
+    enum CodingKeys: CodingKey {
+        case title, code, price, market, fluctuationRate, themas
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = try? container.decodeIfPresent(String.self, forKey: .title)
+        code = try? container.decodeIfPresent(String.self, forKey: .code)
+        market = try? container.decodeIfPresent(MarketType.self, forKey: .market)
+        themas = try? container.decodeIfPresent([String].self, forKey: .themas)
+        price = try? container.decodeIfPresent(Int.self, forKey: .price)
+        fluctuationRate = try? container.decodeIfPresent(Double.self, forKey: .fluctuationRate)
+    }
+    
+    init(
+        title: String?,
+        code: String?,
+        price: Int?,
+        market: MarketType?,
+        fluctuationRate: Double?,
+        themas: [String]?
+    ) {
+        self.title = title
+        self.code = code
+        self.price = price
+        self.market = market
+        self.fluctuationRate = fluctuationRate
+        self.themas = themas
+    }
 }
