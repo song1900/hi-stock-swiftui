0735ff6ee8d26f4e7d4b6d095ce30e4d5da8620f
Woojin Song <woojin1900@gmail.com>
2024-06-24 17:59:17 +0900

    Feat: SearchView List 추가
    - StockChange enum에 Color를 반환하는 속성 추가
    - Stock Entity FluctuationRate를 소수점 2자리까지 표현하는 String값으로 포맷하는 함수 추가

--- HiStock/HiStock/Enums/StockChange.swift
+++ HiStock/HiStock/Enums/StockChange.swift
@@ -5,10 +5,18 @@
 //  Created by 송우진 on 6/14/24.
 //
 
-import Foundation
+import SwiftUI
 
 enum StockChange {
     case up
     case down
     case unchanged
+    
+    var color: Color {
+        switch self {
+        case .up: return Color.red
+        case .down: return Color.blue
+        case .unchanged: return Color.black
+        }
+    }
 }
--- HiStock/HiStock/Presentaion/Search/Views/SearchView.swift
+++ HiStock/HiStock/Presentaion/Search/Views/SearchView.swift
@@ -12,17 +12,17 @@ struct SearchView: View {
     @State var store: StoreOf<SearchReducer>
     
     var body: some View {
-        VStack(spacing: 0, content: {
+        VStack(spacing: 0) {
             searchTextField
-            Divider()
+            resultListView
             Spacer()
-        })
+        }
     }
 }
 
 extension SearchView {
     var searchTextField: some View {
-        HStack(alignment: .center, spacing: 4, content: {
+        HStack(alignment: .center, spacing: 4) {
             Image(systemName: "magnifyingglass")
             TextField("검색어를 입력해 주세요", text: $store.searchText)
                 .textInputAutocapitalization(.never)
@@ -30,9 +30,19 @@ extension SearchView {
                 .onSubmit {
                     store.send(.performSearch)
                 }
-        })
-        .frame(height: 40)
+        }.frame(height: 40)
         .padding(.horizontal, 11)
+        .overlay(alignment: .bottom) {
+            Divider()
+        }
+    }
+    
+    var resultListView: some View {
+        List {
+            ForEach($store.stocks) {
+                SearchResultListCell(stock: $0)
+            }
+        }.listStyle(.plain)
     }
 }
 
