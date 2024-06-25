64ab1a0da4770e6794c5c110f5f4bcec3d2b497d
Woojin Song <woojin1900@gmail.com>
2024-06-25 19:22:14 +0900

    Feat: FirestoreClient 기능 추가
    market collection의 정보를 불러오는 매서드 추가

--- HiStock/HiStock/Dependency/Services/FirestoreManager.swift
+++ HiStock/HiStock/Dependency/Services/FirestoreManager.swift
@@ -8,6 +8,7 @@ import FirebaseFirestore
 
 protocol FirestoreManaging {
     func fetchStocks(thema: String) async throws -> [Stock]
+    func fetchMakets() async throws -> [Market]
 }
 
 final class FirestoreManager: FirestoreManaging {
@@ -17,6 +18,12 @@ final class FirestoreManager: FirestoreManaging {
     func fetchStocks(thema: String) async throws -> [Stock] {
         let querySnapshot = try await db.collection("stock").whereField("themas", arrayContainsAny: [thema]).getDocuments()
         return try querySnapshot.documents.map({ try $0.data(as: Stock.self )})
+    }
+    
+    // 코스피, 코스닥 정보 조회
+    func fetchMakets() async throws -> [Market] {
+        let querySnapshot = try await db.collection("market").getDocuments()
+        return try querySnapshot.documents.map({ try $0.data(as: Market.self )})
     }
 
 }
