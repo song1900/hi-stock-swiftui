baedea7e2e9d4424c0e0cfa7be1d34dd5c674112
Woojin Song <woojin1900@gmail.com>
2024-06-25 19:22:56 +0900

    Chore: 필요 없는 코드 제거 및 Mock파일 임시 수정

--- HiStock/HiStockTests/Mocks/MockFirestoreManager.swift
+++ HiStock/HiStockTests/Mocks/MockFirestoreManager.swift
@@ -9,6 +9,10 @@ import FirebaseFirestore
 @testable import HiStock
 
 final class MockFirestoreManager: FirestoreManaging {
+    func fetchMakets() async throws -> [Market] {
+        return []
+    }
+    
     
     func fetchStocks(thema: String) async throws -> [Stock] {
         return [Stock(title: "테스트 주식", code: nil, price: nil, market: nil, fluctuationRate: nil, themas: [thema])]
